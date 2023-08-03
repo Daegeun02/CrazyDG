f2d2f7bef8f7347593205027e14293fdbb7710cd
Daegeun02 <redhawkdg02@gmail.com>
2023-08-03 10:59:51 +0900

    resolve undetected util files

--- CrazyDG/__init__.py
+++ CrazyDG/__init__.py
@@ -1,9 +1,11 @@
-version = "__0.0.1__"
+version = "__0.0.2__"
 
 from .control import Controller
 
 from .crazy import CrazyDragon
 
 from .guidance import Guidance
+
+from .guidance import utils
 
 from .navigation import Navigation
